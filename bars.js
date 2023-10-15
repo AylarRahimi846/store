e3921ca95f83b62c47aa2a44d7064543cd6a7d57
AylarRahimi846 <rahimiaylar846@gmail.com>
2023-10-15 12:45:49 +0330

    chang

--- bars.js
+++ bars.js
@@ -3,29 +3,7 @@ function showCategory() {
     category.classList.toggle("showCategory")
 }
 
-// function saba(products) {
-//     let cards = products.map(product => {
-//         const { image, title, price, id } = product
-//         return `
-//         <a href="#" onclick="showProduct(${id})" href="#">
-//          <div class = "section__card">
-//     <img class = "section__card--img" src="${image}" alt="">
-//     <h1 class="section__card--title">${title}</h1>
-//     <span class="section__card--price"> price: ${price} $</span>
-//     </div>
-//     </a>
-//     `
-//     })
-//    categorySection.innerHTML = cards.join('')
 
-//    sectionBtn.classList.add("section__btnDisplay");
-//    firstSlide.classList.add("imgDisplay");
-//    sectionCard.classList.add("sectionDisplay");
-//    primary.classList.add("primaryDisplay");
-// }
-
-
-// show category name
 
 fetch('https://fakestoreapi.com/products/categories')
     .then(res => res.json())
